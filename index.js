e81c88003abd960b8601de6887760ef2e334da45
prmstrgt <pramestiregitaa@gmail.com>
2020-09-09 17:22:08 +0700

    create path /items with GET and POST method on index.js

---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./src/helper/db')
+const qs = require('querystring')
+
+const app = express()
+
+//middleware
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/items', (req, res)=>{
+    const {name, price, description} = req.body
+    if(name && price && description){
+        db.query(`INSERT INTO items (name, price, description) VALUES ('${name}', ${price}, '${description}')`, (err, result, fields)=>{
+            if(!err){
+                res.status(201).send({
+                    success: true,
+                    message: 'Item has been created',
+                    data: req.body,
+                })
+            }else{
+                res.status(500).send({
+                    success: false,
+                    message: 'Internal Server Error',
+                })
+            }
+        })
+    }else{
+        console.log(err)
+        res.status(400).send({
+            success: false,
+            message: 'All field must be filled!'
+        })
+    }
+})
+
+app.get('/items', (req, res)=>{
+    let {page, limit, search} = req.query
+    let searchKey = ''
+    let searchValue = ''
+    if(typeof search == 'object'){
+        searchKey = Object.keys(search)[0]
+        searchValue = Object.values(search)[0]
+    }else{
+        searchKey = 'name'
+        searchValue = search || ''
+    }
+    
+    if(!limit){
+        limit = 5
+    }else{
+        limit = parseInt(limit)
+    }
+    if(!page){
+        page = 1
+    }else{
+        page = parseInt(page)
+    }
+    const offset = (page-1) * limit
+
+    db.query(`SELECT * FROM items WHERE ${searchKey} LIKE '%${searchValue}%' LIMIT ${limit} OFFSET ${offset}`, (err, result, fields)=>{
+        if(!err){
+            const pageInfo = {
+                count: 0,
+                pages: 0,
+                currentPage: page,
+                limitPerPage: limit,
+                nextLink: null,
+                prevLink: null,
+            }
+            if(result.length){
+                db.query(`SELECT COUNT(*) AS count FROM items WHERE ${searchKey} LIKE '%${searchValue}%'`, (err, data, fields)=>{
+                    const {count} = data[0]
+                    pageInfo.count = count
+                    pageInfo.pages = Math.ceil(count / limit)
+
+                    const {pages, currentPage} = pageInfo
+
+                    if(currentPage < pages){
+                        pageInfo.nextLink = `http://localhost:8080/items?${qs.stringify({...req.query, ...{page: page+1}})}`
+                    }
+
+                    if(currentPage > 1){
+                        pageInfo.prevLink = `http://localhost:8080/items?${qs.stringify({...req.query, ...{page: page-1}})}`
+                    }
+
+                    res.send({
+                        success: true,
+                        message: 'List of items',
+                        data: result,
+                        pageInfo
+                    })
+                })
+            }else{
+                res.send({
+                    success: true,
+                    message: 'There is no item in list',
+                    pageInfo
+                })
+            }
+        }else{
+            console.log(err)
+            res.status(500).send({
+                success: false,
+                message: 'Internal Server Error'
+            })
+        }
+    })
+})
+
+app.listen(8080, ()=>{
+    console.log('App listening on port 8080')
+})
