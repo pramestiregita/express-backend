4281f324d00d58a895e2540b371c20d7d8b3e967
prmstrgt <pramestiregitaa@gmail.com>
2020-09-12 09:32:45 +0700

    edit file index.js to create path /category

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const itemsRouter = require('./src/routes/items')
+const categoryRouter = require('./src/routes/category')
 
 const app = express()
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/items', itemsRouter)
+app.use('/category', categoryRouter)
 
 app.listen(8080, () => {
   console.log('App listening on port 8080')
