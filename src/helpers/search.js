0e6d0d3ae7396fc62db366f9ba819c2594050c38
prmstrgt <pramestiregitaa@gmail.com>
2020-09-24 23:24:48 +0700

    create search and sort helpers

---
+++ src/helpers/search.js
@@ -0,0 +1,11 @@
+module.exports = (search) => {
+  if (typeof search === 'object') {
+    const searchKey = Object.keys(search)[0]
+    const searchValue = Object.values(search)[0]
+    return { searchKey, searchValue }
+  } else {
+    const searchKey = 'name'
+    const searchValue = search || ''
+    return { searchKey, searchValue }
+  }
+}
