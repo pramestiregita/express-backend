8cdf54484401891a28050de67b99661203a1c35c
prmstrgt <pramestiregitaa@gmail.com>
2020-09-28 09:16:46 +0700

    unused files

--- src/routes/public/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-const route = require('express').Router()
-const { authAdminController, authSellerController, authCustController } = require('../../controllers/authController')
-
-route.post('/login/admin', authAdminController) // admin
-route.post('/login/seller', authSellerController) // seller
-route.post('/login/customer', authCustController) // customer
-
-module.exports = route
--- src/routes/public/registerRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const usersController = require('../../controllers/usersController')
-
-router.post('/', usersController.create) // users and user_details
-
-module.exports = router
